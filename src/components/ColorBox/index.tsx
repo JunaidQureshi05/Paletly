e617d6bc955278eedf4c2dc42b730eccaa51cc6e
Junaid Qureshi <Junaid.562000@gmail.com>
2025-06-12 23:36:27 +0530

    style color box

--- src/components/ColorBox/index.tsx
+++ src/components/ColorBox/index.tsx
@@ -3,7 +3,13 @@ import "./ColorBox.css";
 const ColorBox = ({ color }: { color: { color: string; name: string } }) => {
   return (
     <div style={{ backgroundColor: color.color }} className="ColorBox">
-      <span>MORE</span>
+      <div className="copy-container">
+        <div className="box-content">
+          <span>{color.name}</span>
+        </div>
+        <button className="copy-button">Copy</button>
+      </div>
+      <span className="see-more">More</span>
     </div>
   );
 };
