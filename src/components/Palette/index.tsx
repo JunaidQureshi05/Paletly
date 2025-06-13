ccf627c3ec886452c0271388745f82bbe9634e48
Junaid Qureshi <Junaid.562000@gmail.com>
2025-06-13 14:14:32 +0530

    style color slider

--- src/components/Palette/index.tsx
+++ src/components/Palette/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import ColorBox from "../ColorBox";
+
 import "./Palette.css";
 import { GeneratedPallete } from "../../types/Colors";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import ColorBox from "../ColorBox";
 
 const Palette = ({ palette }: { palette: GeneratedPallete }) => {
   const [level, setLevel] = useState<number>(500);
@@ -12,15 +13,18 @@ const Palette = ({ palette }: { palette: GeneratedPallete }) => {
   return (
     <div className="Palette">
       {/* Navbar goes here */}
-      <Slider
-        defaultValue={level}
-        min={100}
-        max={900}
-        step={100}
-        onChangeComplete={(val: number | number[]) => {
-          setLevel(val as number);
-        }}
-      />
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onChangeComplete={(val: number | number[]) => {
+            setLevel(val as number);
+          }}
+        />
+      </div>
+
       <div className="Palette-colors">
         {palette.colors[level].map((color) => (
           <ColorBox color={color} />
