ec8337fc655dc282800903e1566db9a955e83bfe
Junaid Qureshi <Junaid.562000@gmail.com>
2025-06-13 13:13:46 +0530

    add color helper to generate palette

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import Palette from "./components/Palette";
 import seedColor from "./seedColor";
+import { generatePalette } from "./utils/color";
 function App() {
+  console.log("@@@@", generatePalette(seedColor[2]));
   return (
     <div>
       <Palette {...seedColor[2]} />
