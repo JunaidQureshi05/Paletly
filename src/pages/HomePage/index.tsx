7645178a628252d8f1ce5d6b40ab13229195445f
Junaid Qureshi <Junaid.562000@gmail.com>
2025-06-15 22:21:46 +0530

    start styling PaletteList withStyles

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,23 +2,59 @@ import React from "react";
 import seedColor from "../../seedColor";
 import { Link } from "react-router";
 import MiniPalette from "../../components/MiniPalette";
-const HomePage = () => {
+import { withStyles } from "@mui/styles";
+
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  } as const,
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  } as const,
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white",
+  } as const,
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%",
+  } as const,
+};
+const HomePage = (props: any) => {
+  const { classes } = props;
   return (
-    <div>
-      <h1>Paletly</h1>
-      <div>
-        {seedColor.map((palette) => (
-          <Link to={{ pathname: `/palette/${palette.id}` }} key={palette.id}>
-            <MiniPalette
-              name={palette.paletteName}
-              key={palette.id}
-              emoji={palette.emoji}
-            />
-          </Link>
-        ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>Paletly</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {seedColor.map((palette) => (
+            <Link to={{ pathname: `/palette/${palette.id}` }} key={palette.id}>
+              <MiniPalette
+                name={palette.paletteName}
+                key={palette.id}
+                emoji={palette.emoji}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default HomePage;
+export default withStyles(styles)(HomePage);
